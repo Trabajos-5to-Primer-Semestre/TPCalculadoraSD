85d36a53f4b89d16bc93c86bba08c37242b92ee4
octav <15966899+OrionVi1998@users.noreply.github.com>
2023-03-27 19:52:23 -0300

    Percictencia del servidor

--- mainServer.py
+++ mainServer.py
@@ -5,33 +5,36 @@ PORT = 6666
 
 # Creates socket class element specifying the address family and socket type
 # Socket_stream = TCP
+
+print('Inicializando socket')
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # Binds an IP address and a port number to a socket instance and since the IP is empty it takes all ips
     s.bind(('127.0.0.1', PORT))
     # Listens for connection attempts
     s.listen()
-    # Accepts the socket connection
-    conn, addr = s.accept()
 
-    with conn:  # With the open connection
-        print(f"Connected by {addr}")
+    while True:
+        # Accepts the socket connection
+        conn, addr = s.accept()
 
-        while True:
+        with conn:  # With the open connection
+            print(f"Connected by {addr}")
 
-            # Receive the socket message, specifying the max amount of data
-            data = conn.recv(1024)
-            # The received data is then deserialized into a string
-            data = str(data.decode('utf-8'))
-            print(data)
-            if data == 'no':
-                print("Apagando servidor...")
-                break
+            while True:
+                # Receive the socket message, specifying the max amount of data
+                data = conn.recv(1024)
+                # The received data is then deserialized into a string
+                data = str(data.decode('utf-8'))
+                print(data)
+                if data == 'no':
+                    print("Cerrando conexion...")
+                    break
 
-            # Perform the calculations needed
-            data = str(numexpr.evaluate(data.replace('^', '**')))
-            # Serialize the data for sending back to the client
-            data = bytes(data, encoding='utf8')
+                # Perform the calculations needed
+                data = str(numexpr.evaluate(data.replace('^', '**')))
+                # Serialize the data for sending back to the client
+                data = bytes(data, encoding='utf8')
 
-            conn.sendall(data)
+                conn.sendall(data)
 
 
